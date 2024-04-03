993bc85410ea298e043060c2ed77aa5e246e6649
angel bermudez <9015@holbertonstudents.com>
2024-04-02 20:33:32 -0400

    fixxed betty flags

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
  * @parent: A pointer to the node to insert the left-child in
  * @value: The value to store in the new node
  *
- * Return: A pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: A pointer to the created node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -13,12 +13,12 @@ binary_tree_t *new_node = NULL;
 
 	if (parent == NULL)
 {
-	return NULL;
+	return (NULL);
 }
 new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 {
-	return NULL;
+	return (NULL);
 }
 	new_node->n = value;
 	new_node->left = parent->left;
@@ -29,5 +29,5 @@ new_node = malloc(sizeof(binary_tree_t));
 	parent->left->parent = new_node;
 }
 parent->left = new_node;
-    return new_node;
+	return (new_node);
 }
