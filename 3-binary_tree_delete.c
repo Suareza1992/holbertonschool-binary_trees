73bc5bd8921d1a80878e7746550e062366ffe8e9
angel bermudez <9015@holbertonstudents.com>
2024-04-04 16:33:53 -0400

    added return line

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,16 @@
 #include "binary_trees.h"
 
-/*
+/**
 *binary_tree_delete - function to delete a binary tree
 *@tree: Tree to be deleted
-*Return: void if successful
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
 if (tree == NULL)
+{
+return;
+}
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
 free(tree);
-    return (void);
 }
