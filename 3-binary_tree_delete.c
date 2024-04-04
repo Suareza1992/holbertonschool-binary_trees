8a667dd86a56ec1fa98f894978e4878ef88d128b
angel bermudez <9015@holbertonstudents.com>
2024-04-04 16:24:28 -0400

    betty issues

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 
-/*
+/**
 *binary_tree_delete - function to delete a binary tree
 *@tree: Tree to be deleted
-*Return: void if successful
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
@@ -11,6 +10,5 @@ if (tree == NULL)
 binary_tree_delete(tree->left);
 binary_tree_delete(tree->right);
 free(tree);
-    return (void);
 }
 
